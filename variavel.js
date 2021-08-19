52d17994576a85f9263ec7850c037ddbbec01062
Augusta Stroeher <88950277+gutastr@users.noreply.github.com>
2021-08-18 23:38:11 -0300

    Create variavel.js

---
+++ variavel.js
@@ -0,0 +1,11 @@
+nomeCompleto = 'Augusta Stroeher';
+apelido = 'Guta';
+idade = 44;
+dataNascimento = '25/04/1977';
+localNascimento = 'Feliz/RS';
+altura = 1.67;
+trabalhando = false;
+
+apresentacao = "Meu nome é " + nomeCompleto + " (sou conhecida como " + apelido + ") e tenho " + idade + " anos. Nasci no dia " + dataNascimento + ", na cidade de " + localNascimento + ". Tenho " + altura + "m de altura e atualmente estou " + (trabalhando ? "empregada" : "desempregada") + "."
+
+console.log(apresentacao);
